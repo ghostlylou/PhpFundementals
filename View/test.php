484c669d8590ac2d08d3c6d0995b07572f71fce6
Lou <louellacreemers@gmail.com>
2021-03-30 18:15:06 +0200

    Update test.php

--- View/test.php
+++ View/test.php
@@ -5,31 +5,33 @@ require_once './Model/PaymentModel.php';
 
 ini_set('display_errors', -1);
 
-$paymentService = new PaymentService();
-////$payment = new PaymentModel(15, 'paid', 'louellacreemers@gmail.com');
-////
-////$paymentService->createPayment($payment);
-//
-//$returnPayment = $paymentService->getPaymentByMail('louellacreemers@gmail.com');
-//
-//$id = $returnPayment->getId();
-//
-//echo "id=".$id;
-
-
-//$email = new emailOrderGen();
-//
-//$email->sendEmail(4, 'paid', 'louellacreemers@gmail.com');
-
 echo "empty";
-
-$pArray = $paymentService->getDistinctEmails();
-
-var_dump($pArray);
-
-if(in_array("louellacreemers@gmail.com", $pArray)){
-    echo "Found it";
-}
+?>
+//$paymentService = new PaymentService();
+//////$payment = new PaymentModel(15, 'paid', 'louellacreemers@gmail.com');
+//////
+//////$paymentService->createPayment($payment);
+////
+////$returnPayment = $paymentService->getPaymentByMail('louellacreemers@gmail.com');
+////
+////$id = $returnPayment->getId();
+////
+////echo "id=".$id;
+//
+//
+////$email = new emailOrderGen();
+////
+////$email->sendEmail(4, 'paid', 'louellacreemers@gmail.com');
+//
+//echo "empty";
+//
+//$pArray = $paymentService->getDistinctEmails();
+//
+//var_dump($pArray);
+//
+//if(in_array("louellacreemers@gmail.com", $pArray)){
+//    echo "Found it";
+//}
 
 else{
     echo "no";

8c88e098fa140889ab6a46ef3b3d73bc5a398eb3
Lou <louellacreemers@gmail.com>
2021-04-01 23:42:11 +0200

    push

--- View/webhook.php
+++ View/webhook.php
@@ -6,8 +6,8 @@ require_once "../lib/mollie/vendor/autoload.php";
 require_once "../Controller/PaymentController.php";
 require_once "../Email/mailer.php";
 require_once "../pdf/emailOrderGen.php";
-$amount = $_GET['amount'];
-$email = $_GET['email'];
+$amount = 403;
+$email = "louellacreemers@gmail.com";
 
 $paymentController = new PaymentController();
 $emailArray = $paymentController->getDistinctEmails();
@@ -20,8 +20,8 @@ $mollie = new MollieApiClient();
 $mollie->setApiKey("test_BJqCEmBVqfHW8nWxDsAmk58SRcNWhP");
 
 $payment = $mollie->payments->get($paymentId);
-
-if ($payment->isPaid()){
+//
+//if ($payment->isPaid()){
     $emailGen = new emailOrderGen();
 
     $paymentController->createPayment($amount, "paid", $email);
@@ -29,11 +29,11 @@ if ($payment->isPaid()){
 
     $emailGen->sendEmail($amount, "paid", $email);
     $mailer->sendMail("louellacreemers@gmail.com", "A donation has been created", "Someone posted a donation of $amount");
-}
+//}
 
-else{
+//else{
     $mailer->sendMail($email, "Donation error: Your payment failed", "Thank you for trying to donate! Please try another email address or try again later");
 
-}
+//
 
 
